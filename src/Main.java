2d58cb521c5af21df0d05caceea2fc34d090a900
Bishwajit-Saha <bishwo.saha@gmail.com>
2017-11-28 20:33:42 +0530

    2 added user id info

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,9 @@ class Main{
 
 	public static void main(Stings[] args)
 	{
+		int a = 10;
 		System.out.println("Hello " + args[0]);
 		System.out.println("Hope you are having a good time");
+		System.out.println("Your id is: " + a);
 	}
 }
